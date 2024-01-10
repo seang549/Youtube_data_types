d7f3425ddd8e168a208be000b9bd8c6de449bfd0
Sean Guerrero <seanguerrero549@gmail.com>
2024-01-09 18:14:51 -0800

    Code covered

---
+++ script.js
@@ -0,0 +1,32 @@
+// let obj =  {
+//     age: 25,
+//     name: "John Doe",
+//     isLogged: true,
+
+// }
+
+// let arr = [1, 2, [3, 4]]
+// // console.log(arr[2][1])
+
+// let age = 23;
+
+// let name = 'Coding With Sean';
+
+// let isVideo = true;
+
+// let city;
+
+// let car = null;
+
+// console.log(age);
+// console.log(name);
+// console.log(isVideo);
+// console.log(city);
+// console.log(car);
+
+
+
+// const a = [true]
+// const b = a
+// b[0] = false
+// console.log(a)
